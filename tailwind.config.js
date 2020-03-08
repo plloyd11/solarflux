50353107bf2280ef296437bc83417878beb0c32e
Pete Lloyd <plloyd11@gmail.com>
2020-03-08 13:16:10 -0400

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,35 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
+    fontFamily: {
+      display: ['Montserrat', 'sans-serif'],
+      body: ['Merriweather', 'serif'],
+    },
+    borderWidth: {
+      default: '1px',
+      '0': '0',
+      '2': '2px',
+      '4': '4px',
+    },
+    extend: {
+      colors: {
+        cyan: '#9cdbff',
+        primaryPurple: '#16134C',
+        primaryOrange: '#F04822',
+      },
+      spacing: {
+        '96': '24rem',
+        '128': '32rem',
+      }
+    },
+    plugins: [
+      require('@tailwindcss/ui')
+    ],
+  }
+}
